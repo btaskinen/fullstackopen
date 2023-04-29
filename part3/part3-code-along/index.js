b1c49f783bcc7b48bed1d482ecb583362007721d
Barbara Taskinen <barbara.taskinen@gmail.com>
2023-04-29 08:06:29 +0300

    Code along part3-2.

--- part3/part3-code-along/index.js
+++ part3/part3-code-along/index.js
@@ -1,6 +1,20 @@
 const express = require("express");
 const app = express();
 
+// middleware
+const requestLogger = (request, response, next) => {
+  console.log("Method:", request.method);
+  console.log("Path", request.path);
+  console.log("Body", request.body);
+  console.log("---");
+  next();
+};
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: "unknown endpoint" });
+};
+
+app.use(express.json());
 app.use(express.json());
 
 let notes = [
@@ -69,6 +83,8 @@ app.delete("/api/notes/:id", (request, response) => {
   notes = notes.filter((note) => note.id !== id);
   response.status(204).end();
 });
+
+app.use(unknownEndpoint);
 
 const PORT = 3001;
 app.listen(PORT);
