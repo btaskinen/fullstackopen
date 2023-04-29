c9a5337993391d1a8834df4bb7d2c6fef8900611
Barbara Taskinen <barbara.taskinen@gmail.com>
2023-04-29 08:25:15 +0300

    Exercise 3.8.

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -2,8 +2,12 @@ const express = require("express");
 const morgan = require("morgan");
 const app = express();
 
+morgan.token("request-body", (request, response) => {
+  if (request.body.name) return JSON.stringify(request.body);
+});
+
 app.use(express.json());
-app.use(morgan("tiny"));
+app.use(morgan(":method :url :response-time :request-body "));
 
 let persons = [
   {
