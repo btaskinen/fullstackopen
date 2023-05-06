9739b25a649501a323053efd9df530f02bb31e30
Barbara Taskinen <barbara.taskinen@gmail.com>
2023-05-06 09:12:30 +0300

    code along part3b.

--- part3/part3-code-along/index.js
+++ part3/part3-code-along/index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const app = express();
+const cors = require("cors");
 
 // middleware
 const requestLogger = (request, response, next) => {
@@ -14,6 +15,8 @@ const unknownEndpoint = (request, response) => {
   response.status(404).send({ error: "unknown endpoint" });
 };
 
+app.use(express.static("build"));
+app.use(cors());
 app.use(express.json());
 app.use(express.json());
 
@@ -86,6 +89,7 @@ app.delete("/api/notes/:id", (request, response) => {
 
 app.use(unknownEndpoint);
 
-const PORT = 3001;
-app.listen(PORT);
-console.log(`Server running on port ${PORT}`);
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
