b36e240ca151e393fdb7616648279ec58621dec0
Barbara Taskinen <barbara.taskinen@gmail.com>
2023-05-06 15:05:20 +0300

    Exercise 3.11.

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -7,6 +7,7 @@ morgan.token("request-body", (request, response) => {
   if (request.body.name) return JSON.stringify(request.body);
 });
 
+app.use(express.static("build"));
 app.use(cors());
 app.use(express.json());
 app.use(morgan(":method :url :response-time :request-body "));
