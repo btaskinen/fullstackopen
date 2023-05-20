036765b7c8b99c4b507c809290d01e9b119b71e9
Barbara Taskinen <barbara.taskinen@gmail.com>
2023-05-20 09:00:44 +0300

    part3d code along lint.

--- part3/part3-code-along/mongo.js
+++ part3/part3-code-along/mongo.js
@@ -1,23 +1,23 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose')
 
 if (process.argv.length < 3) {
-  console.log("give password as argument");
-  process.exit(1);
+  console.log('give password as argument')
+  process.exit(1)
 }
 
-const password = process.argv[2];
+const password = process.argv[2]
 
-const url = `mongodb+srv://fullstackopenbt:${password}@cluster0.a1c0bw8.mongodb.net/noteApp?retryWrites=true&w=majority`;
+const url = `mongodb+srv://fullstackopenbt:${password}@cluster0.a1c0bw8.mongodb.net/noteApp?retryWrites=true&w=majority`
 
-mongoose.set("strictQuery", false);
-mongoose.connect(url);
+mongoose.set('strictQuery', false)
+mongoose.connect(url)
 
 const noteShema = new mongoose.Schema({
   content: String,
   important: Boolean,
-});
+})
 
-const Note = mongoose.model("Note", noteShema);
+const Note = mongoose.model('Note', noteShema)
 
 // const note = new Note({
 //   content: "Today we have a test",
@@ -31,7 +31,7 @@ const Note = mongoose.model("Note", noteShema);
 
 Note.find({ important: true }).then((result) => {
   result.forEach((note) => {
-    console.log(note);
-  });
-  mongoose.connection.close();
-});
+    console.log(note)
+  })
+  mongoose.connection.close()
+})
