e868895783d37d17da36193c74ee91ea4b367c62
Barbara <barbara.taskinen@gmail.com>
2023-07-04 15:04:50 +0300

    Reorganize backend of notes-app

---
+++ part4/notes-app/mongo.js
@@ -0,0 +1,37 @@
+const mongoose = require('mongoose')
+
+if (process.argv.length < 3) {
+  console.log('give password as argument')
+  process.exit(1)
+}
+
+const password = process.argv[2]
+
+const url = `mongodb+srv://fullstackopenbt:${password}@cluster0.a1c0bw8.mongodb.net/noteApp?retryWrites=true&w=majority`
+
+mongoose.set('strictQuery', false)
+mongoose.connect(url)
+
+const noteShema = new mongoose.Schema({
+  content: String,
+  important: Boolean,
+})
+
+const Note = mongoose.model('Note', noteShema)
+
+// const note = new Note({
+//   content: "Today we have a test",
+//   important: false,
+// });
+
+// note.save().then((result) => {
+//   console.log("note saved!");
+//   mongoose.connection.close();
+// });
+
+Note.find({ important: true }).then((result) => {
+  result.forEach((note) => {
+    console.log(note)
+  })
+  mongoose.connection.close()
+})
