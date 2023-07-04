1c978c4f6b128e005eb2720240583cddddfd2e0c
Barbara <barbara.taskinen@gmail.com>
2023-07-04 16:21:56 +0300

    Exercise 4.1: Get the server running
    - add dotenv package to securely handle passwords
    - index.js: move database configurations into own module
    - blog-entry.js: create module for database configurations

--- part4/blog-list/backend/index.js
+++ part4/blog-list/backend/index.js
@@ -1,22 +1,21 @@
+require('dotenv').config();
 const express = require('express');
 const app = express();
 const cors = require('cors');
-const mongoose = require('mongoose');
-
-const blogSchema = new mongoose.Schema({
-  title: String,
-  author: String,
-  url: String,
-  likes: Number,
-});
-
-const Blog = mongoose.model('Blog', blogSchema);
-
-const mongoUrl = 'mongodb://localhost/bloglist';
-mongoose.connect(mongoUrl);
+const Blog = require('./models/blog-entry');
 
 app.use(cors());
 app.use(express.json());
+
+app.get('/info', (request, response) => {
+  const timeOfRequest = new Date();
+  response.send(
+    `
+    <p>The Blog List is a list of exciting blogs.<p>
+    <p>${timeOfRequest}<p>
+    `
+  );
+});
 
 app.get('/api/blogs', (request, response) => {
   Blog.find({}).then((blogs) => {
@@ -32,7 +31,7 @@ app.post('/api/blogs', (request, response) => {
   });
 });
 
-const PORT = 3003;
+const PORT = process.env.PORT;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
