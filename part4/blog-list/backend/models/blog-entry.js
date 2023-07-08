8c72ffe380aa0927b4e0ed15c6d238e61999db15
Barbara <barbara.taskinen@gmail.com>
2023-07-08 14:38:55 +0300

    Add validation for URL

--- part4/blog-list/backend/models/blog-entry.js
+++ part4/blog-list/backend/models/blog-entry.js
@@ -17,7 +17,17 @@ mongoose
 const blogSchema = new mongoose.Schema({
   title: String,
   author: String,
-  url: String,
+  url: {
+    type: String,
+    validate: {
+      validator: function (v) {
+        return /(https:\/\/www\.|http:\/\/www\.|https:\/\/|http:\/\/)?[a-zA-Z0-9]{2,}(\.[a-zA-Z0-9]{2,})(\.[a-zA-Z0-9]{2,})?\/[a-zA-Z0-9]{2,}/.test(
+          v
+        );
+      },
+      message: (props) => `"${props.value}" is not a valid URL!`,
+    },
+  },
   likes: Number,
 });
 
