332a6026a63fa25b42358a414272fa55c6425a80
Barbara <barbara.taskinen@gmail.com>
2023-07-25 15:26:44 +0300

    code along part 4 Testing Node applications

--- part4/notes-app/.eslintrc.js
+++ part4/notes-app/.eslintrc.js
@@ -4,6 +4,7 @@ module.exports = {
     commonjs: true,
     es2021: true,
     node: true,
+    jest: true,
   },
   extends: 'eslint:recommended',
   parserOptions: {
@@ -19,4 +20,4 @@ module.exports = {
     'arrow-spacing': ['error', { before: true, after: true }],
     'no-console': 0,
   },
-}
+};
