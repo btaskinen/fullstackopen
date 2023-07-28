c29adcc477361d2592f7f962d9fe0982f7310aac
Barbara <barbara.taskinen@gmail.com>
2023-07-28 15:19:47 +0300

    Exercise 4.9: write test to see that id property of the blog posts is named id

--- part4/blog-list/backend/tests/blog-list.test.js
+++ part4/blog-list/backend/tests/blog-list.test.js
@@ -29,6 +29,12 @@ test('all blogs are returned', async () => {
   expect(response.body).toHaveLength(helper.initialBlogs.length);
 });
 
+test('id property is correctly formatted', async () => {
+  const response = await api.get('/api/blogs');
+
+  expect(response.body[0].id).toBeDefined();
+});
+
 afterAll(async () => {
   await mongoose.connection.close();
 });
