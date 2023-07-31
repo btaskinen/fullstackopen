745dbfab290dce4ea3e43569ae9666398cf8d340
Barbara <barbara.taskinen@gmail.com>
2023-07-31 12:34:45 +0300

    Exercise 4.14: Refactor PUT route handler

--- part4/blog-list/backend/controllers/blogs.js
+++ part4/blog-list/backend/controllers/blogs.js
@@ -35,18 +35,15 @@ blogsRouter.post('/', async (request, response) => {
   response.status(201).json(savedBlog);
 });
 
-blogsRouter.put('/:id', (request, response, next) => {
+blogsRouter.put('/:id', async (request, response) => {
   const { title, author, url, likes } = request.body;
 
-  Blog.findByIdAndUpdate(
+  const updatedBlog = await Blog.findByIdAndUpdate(
     request.params.id,
     { title, author, url, likes },
     { new: true }
-  )
-    .then((updatedBlog) => {
-      response.status(200).json(updatedBlog);
-    })
-    .catch((error) => next(error));
+  );
+  response.status(200).json(updatedBlog);
 });
 
 blogsRouter.delete('/:id', async (request, response) => {
