37645806abacda4013f1f84edfb827301a9d5cc3
Barbara <barbara.taskinen@gmail.com>
2023-08-08 15:28:26 +0300

    Exercise 5.7: Implement visibility toggle for blog details

--- part4/blog-list/frontend/src/components/Blog.js
+++ part4/blog-list/frontend/src/components/Blog.js
@@ -1,46 +1,64 @@
 /* eslint-disable react/prop-types */
-import React from 'react';
+import { React, useState } from 'react';
 import './Blog.css';
 
 const Blog = ({ index, blog, deleteBlog, addLike }) => {
+  const [blogDetailsVisible, setVisibility] = useState(false);
   const backgroundColor = index % 2 === 0 ? 'dark' : 'light';
+
+  const handleVisibility = () => {
+    setVisibility(!blogDetailsVisible);
+  };
+
+  const displayDetails = { display: blogDetailsVisible ? '' : 'none' };
+  const buttonLabel = blogDetailsVisible ? 'Hide' : 'View';
 
   console.log('Inside Blog', blog);
 
   return (
     <div key={blog._id} className={`Blog ${backgroundColor}`}>
-      <p>
-        <strong>Title:</strong>
-      </p>
-      <p>{blog.title}</p>
-      <p>
-        <strong>Author:</strong>
-      </p>
-      <p>{blog.author}</p>
-      <p>
-        <strong>URL:</strong>
-      </p>
-      <p>
-        <a href={blog.url}>{blog.url}</a>
-      </p>
-      <p>
-        <strong>Likes:</strong>
-      </p>
-      <p>{blog.likes}</p>
-      <button
-        type="button"
-        className="Blog_Button"
-        onClick={() => addLike(blog)}
-      >
-        Like
-      </button>
-      <button
-        type="button"
-        onClick={() => deleteBlog(blog)}
-        className="Blog_Button"
-      >
-        Delete
-      </button>
+      <div className="Blog_title">
+        <p>
+          <strong>{blog.title}</strong>
+        </p>
+        <button
+          className="Blog_Button Blog_viewButton"
+          onClick={handleVisibility}
+        >
+          {buttonLabel}
+        </button>
+      </div>
+
+      <div className="Blog_details" style={displayDetails}>
+        <p>
+          <strong>Author:</strong>
+        </p>
+        <p className="Blog_authorValue">{blog.author}</p>
+        <p>
+          <strong>URL:</strong>
+        </p>
+        <p className="Blog_urlValue">
+          <a href={blog.url}>{blog.url}</a>
+        </p>
+        <p>
+          <strong>Likes:</strong>
+        </p>
+        <p>{blog.likes}</p>
+        <button
+          type="button"
+          className="Blog_Button"
+          onClick={() => addLike(blog)}
+        >
+          Like
+        </button>
+        <button
+          type="button"
+          onClick={() => deleteBlog(blog)}
+          className="Blog_Button"
+        >
+          Delete
+        </button>
+      </div>
     </div>
   );
 };
