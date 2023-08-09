e823e242c6dfb4179efa7b3efcd16df8510b1a68
Barbara <barbara.taskinen@gmail.com>
2023-08-09 15:25:43 +0300

    Exercise 5.14: Test that blog details are shown after clicking 'View'-button

--- part4/blog-list/frontend/src/components/Blog.test.js
+++ part4/blog-list/frontend/src/components/Blog.test.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import '@testing-library/jest-dom/extend-expect';
 import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import Blog from './Blog';
 
 test('renders only blog title', () => {
@@ -17,8 +18,6 @@ test('renders only blog title', () => {
 
   const { container } = render(<Blog blog={blog} />);
 
-  screen.debug();
-
   const titleDiv = container.querySelector('.Blog_title');
   const detailsDiv = container.querySelector('.Blog_details');
 
@@ -26,4 +25,40 @@ test('renders only blog title', () => {
   expect(titleDiv).not.toHaveStyle('display: none');
   expect(detailsDiv).toHaveStyle('display: none');
   expect(detailsDiv.content).not.toBe('Likes');
+});
+
+test('clicking "View"-button displays all the blog details', async () => {
+  const blog = {
+    title: 'This is a blog about testing',
+    author: 'Test Tester',
+    url: 'http://www.blogs.com/testing101',
+    likes: 3,
+    user: {
+      username: 'ttester',
+      name: 'Test Tester',
+    },
+  };
+
+  const { container } = render(<Blog blog={blog} />);
+
+  const user = userEvent.setup();
+  const viewButton = container.querySelector('.Blog_toggleButton');
+  expect(viewButton).toHaveTextContent('View');
+
+  await user.click(viewButton);
+
+  const div = container.querySelector('.Blog_details');
+  const authorElement = container.querySelector('.Blog_author');
+  const urlElement = container.querySelector('.Blog_url');
+  const userElement = container.querySelector('.Blog_user');
+  const likesElement = container.querySelector('.Blog_likes');
+
+  screen.debug(likesElement);
+
+  expect(div).not.toHaveStyle('display: none');
+  expect(authorElement).toHaveTextContent('Test Tester');
+  expect(urlElement).toHaveTextContent('http://www.blogs.com/testing101');
+  expect(userElement).toHaveTextContent('Test Tester');
+  expect(likesElement).toHaveTextContent(3);
+  expect(viewButton).toHaveTextContent('Hide');
 });
