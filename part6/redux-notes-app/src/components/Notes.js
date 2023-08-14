702d09f73b8689efe3a81a3ec92d5e7b92754618
Barbara <barbara.taskinen@gmail.com>
2023-08-14 15:32:47 +0300

    Code along part 6a

---
+++ part6/redux-notes-app/src/components/Notes.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { useDispatch, useSelector } from 'react-redux';
+import { toggleImportanceOf } from '../reducers/noteReducer';
+
+const Note = ({ note, handleClick }) => {
+  return (
+    <li onClick={handleClick}>
+      {note.content} <strong>{note.important ? 'important' : ''}</strong>
+    </li>
+  );
+};
+
+const Notes = () => {
+  const dispatch = useDispatch();
+  const notes = useSelector((state) => state);
+
+  return (
+    <ul>
+      {notes.map((note) => (
+        <Note
+          key={note.id}
+          note={note}
+          handleClick={() => dispatch(toggleImportanceOf(note.id))}
+        />
+      ))}
+    </ul>
+  );
+};
+
+Note.propTypes = {
+  note: PropTypes.object.isRequired,
+  handleClick: PropTypes.func.isRequired,
+};
+export default Notes;
--- part6/redux-notes-app/src/index.js
+++ part6/redux-notes-app/src/index.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
 import './index.css';
 import App from './App';
+import noteReducer from './reducers/noteReducer';
+
+const store = createStore(noteReducer);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
