bd9a822386adde107727e4fa37cf2272891ccc2f
Barbara <barbara.taskinen@gmail.com>
2023-08-15 10:39:16 +0300

    Code along part 6b: Code with complex state

--- part6/redux-notes-app/src/App.js
+++ part6/redux-notes-app/src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import NewNotes from './components/NewNotes';
 import Notes from './components/Notes';
+import VisibilityFilter from './components/VisibilityFilter';
 
 const App = () => {
   return (
     <div>
       <NewNotes />
+      <VisibilityFilter />
       <Notes />
     </div>
   );
--- part6/redux-notes-app/src/index.js
+++ part6/redux-notes-app/src/index.js
@@ -1,12 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { createStore } from 'redux';
+import { createStore, combineReducers } from 'redux';
 import { Provider } from 'react-redux';
 import './index.css';
 import App from './App';
 import noteReducer from './reducers/noteReducer';
+import filterReducer from './reducers/filterReducer';
 
-const store = createStore(noteReducer);
+const reducer = combineReducers({
+  notes: noteReducer,
+  filter: filterReducer,
+});
+
+const store = createStore(reducer);
+
+console.log(store.getState());
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
