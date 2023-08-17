03e0eddcd7a7a90318a41acaf22fbaa8f46abdeb
Barbara <barbara.taskinen@gmail.com>
2023-08-17 11:15:52 +0300

    Code along part 6d: React Query

--- part6/query-notes/src/App.js
+++ part6/query-notes/src/App.js
@@ -1,18 +1,45 @@
 import React from 'react';
+import { useQuery, useMutation, useQueryClient } from 'react-query';
+import { getNotes, createNote, updateNote } from './requests';
 
 const App = () => {
+  const queryClient = useQueryClient();
+
+  const newNoteMutation = useMutation(createNote, {
+    onSuccess: (newNote) => {
+      const notes = queryClient.getQueryData('notes');
+      queryClient.setQueriesData('notes', notes.concat(newNote));
+    },
+  });
+
+  const updateNoteMutation = useMutation(updateNote, {
+    onSuccess: () => {
+      queryClient.invalidateQueries('notes');
+    },
+  });
   const addNote = async (event) => {
     event.preventDefault();
     const content = event.target.note.value;
     event.target.note.value = '';
     console.log(content);
+    newNoteMutation.mutate({ content, important: true });
   };
 
   const toggleImportance = (note) => {
     console.log('toggle importance of', note.id);
+    updateNoteMutation.mutate({ ...note, important: !note.important });
   };
 
-  const notes = [];
+  const result = useQuery('notes', () => getNotes(), {
+    refetchOnWindowFocus: false,
+  });
+  console.log(result);
+
+  if (result.isLoading) {
+    return <div>loading data...</div>;
+  }
+
+  const notes = result.data;
 
   return (
     <div>
