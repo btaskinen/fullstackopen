819fb8514691215e84f3a617def969abae4e3e85
Barbara <barbara.taskinen@gmail.com>
2023-08-23 12:45:08 +0300

    Code along part 7e: Initial project setup

---
+++ part7/class-anecdotes/src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './App.css';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>anecdote of the day</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
