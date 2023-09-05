ee9b639ab1e5ac48cd674c07d27ee11363c5d886
Barbara <barbara.taskinen@gmail.com>
2023-09-05 11:53:38 +0300

    Code along part 8b

--- part8/phonebook-graphql-frontend/src/App.js
+++ part8/phonebook-graphql-frontend/src/App.js
@@ -1,8 +1,45 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useQuery } from '@apollo/client';
+import Persons from './components/Persons';
 import './App.css';
+import PersonForm from './components/PersonForm';
+import { ALL_PERSONS } from './queries';
+import PropType from 'prop-types';
 
 const App = () => {
-  return <div className="App">Content Here</div>;
+  const [errorMessage, setErrorMessage] = useState(null);
+
+  const result = useQuery(ALL_PERSONS);
+
+  if (result.loading) {
+    return <div>loading...</div>;
+  }
+
+  const notify = (message) => {
+    setErrorMessage(message);
+    setTimeout(() => {
+      setErrorMessage(null);
+    }, 10000);
+  };
+
+  return (
+    <div>
+      <Notify errorMessage={errorMessage} />
+      <Persons persons={result.data.allPersons} />
+      <PersonForm setError={notify} />
+    </div>
+  );
+};
+
+const Notify = ({ errorMessage }) => {
+  if (!errorMessage) {
+    return null;
+  }
+  return <div style={{ color: 'red' }}>{errorMessage}</div>;
+};
+
+Notify.propTypes = {
+  errorMessage: PropType.string,
 };
 
 export default App;
--- part8/phonebook-graphql-frontend/src/index.js
+++ part8/phonebook-graphql-frontend/src/index.js
@@ -3,9 +3,39 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 
+import {
+  ApolloClient,
+  ApolloProvider,
+  InMemoryCache,
+  gql,
+} from '@apollo/client';
+
+const client = new ApolloClient({
+  uri: 'http://localhost:4000',
+  cache: new InMemoryCache(),
+});
+
+const query = gql`
+  query {
+    allPersons {
+      name
+      phone
+      address {
+        street
+        city
+      }
+      id
+    }
+  }
+`;
+
+client.query({ query }).then((response) => {
+  console.log(response.data);
+});
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  <ApolloProvider client={client}>
     <App />
-  </React.StrictMode>
+  </ApolloProvider>
 );
