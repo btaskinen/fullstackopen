638ad27820b37e1194473255d6ae42ccbb5cd52a
Barbara <barbara.taskinen@gmail.com>
2023-09-07 09:32:57 +0300

    Set up project for part 8b exercises

---
+++ part8/library-frontend/src/components/Books.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import PropType from 'prop-types';
+
+const Books = ({ show }) => {
+  if (!show) {
+    return null;
+  }
+
+  const books = [];
+
+  return (
+    <div>
+      <h2>books</h2>
+
+      <table>
+        <tbody>
+          <tr>
+            <th></th>
+            <th>author</th>
+            <th>published</th>
+          </tr>
+          {books.map((a) => (
+            <tr key={a.title}>
+              <td>{a.title}</td>
+              <td>{a.author}</td>
+              <td>{a.published}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+};
+
+Books.propTypes = {
+  show: PropType.bool.isRequired,
+};
+
+export default Books;
