85b8fb0b3b1f0d754f58188d83b2242a24c0b709
Barbara <barbara.taskinen@gmail.com>
2023-09-12 11:57:23 +0300

    Code along part 8c: Friends list

--- part8/phonebook-graphql/index.js
+++ part8/phonebook-graphql/index.js
@@ -72,6 +72,9 @@ const typeDefs = `
       username: String!
       password: String!
     ): Token
+    addAsFriend(
+      name: String
+    ): User
   }
 `;
 
@@ -98,11 +101,22 @@ const resolvers = {
     },
   },
   Mutation: {
-    addPerson: async (root, args) => {
+    addPerson: async (root, args, context) => {
       const person = new Person({ ...args });
+      const currentUser = context.currentUser;
+
+      if (!currentUser) {
+        throw new GraphQLError('not authenticated', {
+          extensions: {
+            code: 'BAD_USER_INPUT',
+          },
+        });
+      }
 
       try {
         await person.save();
+        currentUser.friends = currentUser.firends.concat(person);
+        await currentUser.save();
       } catch (error) {
         throw new GraphQLError('Saving person failed', {
           extensions: {
@@ -161,6 +175,27 @@ const resolvers = {
       };
 
       return { value: jwt.sign(userForToken, process.env.JWT_SECRET) };
+    },
+    addAsFriend: async (root, args, { currentUser }) => {
+      const isFriend = (person) =>
+        currentUser.friends
+          .map((f) => f._id.toString())
+          .includes(person._id.toString());
+
+      if (!currentUser) {
+        throw new GraphQLError('wrong credentials', {
+          extensions: { code: 'BAD_USER_INPUT' },
+        });
+      }
+
+      const person = await Person.findOne({ name: args.name });
+      if (!isFriend(person)) {
+        currentUser.friends = currentUser.friends.concat(person);
+      }
+
+      await currentUser.save();
+
+      return currentUser;
     },
   },
 };
