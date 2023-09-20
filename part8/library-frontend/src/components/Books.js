51b2137e78c7de4b0c0f0e7c9b490cefc4672a05
Barbara <barbara.taskinen@gmail.com>
2023-09-20 12:10:10 +0300

    Exercise 8.19
    - implement filtering of books by genre

--- part8/library-frontend/src/components/Books.js
+++ part8/library-frontend/src/components/Books.js
@@ -1,18 +1,53 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useQuery } from '@apollo/client';
 import { ALL_BOOKS } from '../queries';
 
 const Books = () => {
+  const [filteredBooks, setFilteredBooks] = useState(null);
   const books = useQuery(ALL_BOOKS);
 
   if (books.loading) {
     return <div>loading...</div>;
   }
 
+  let allGenres = ['all'];
+
+  books.data.allBooks.map((b) => {
+    return b.genres.map((g) => allGenres.push(g));
+  });
+
+  const filteredGenres = [...new Set(allGenres)];
+
+  const handleFilter = (genre) => {
+    if (genre === 'all') {
+      setFilteredBooks(books.data.allBooks);
+      return;
+    }
+    const localFilteredBooks = books.data.allBooks.filter((b) =>
+      b.genres.includes(genre)
+    );
+    setFilteredBooks(localFilteredBooks);
+  };
+
+  const booksToDisplay = filteredBooks ? filteredBooks : books.data.allBooks;
+
+  console.log(booksToDisplay);
+
   return (
     <div>
       <h2>books</h2>
-
+      <div>
+        {filteredGenres.map((g) => (
+          <button
+            onClick={() => handleFilter(g)}
+            style={{ margin: 3 }}
+            key={g}
+            type="button"
+          >
+            {g}
+          </button>
+        ))}
+      </div>
       <table>
         <tbody>
           <tr>
@@ -20,7 +55,7 @@ const Books = () => {
             <th>author</th>
             <th>published</th>
           </tr>
-          {books.data.allBooks.map((a) => (
+          {booksToDisplay.map((a) => (
             <tr key={a.title}>
               <td>{a.title}</td>
               <td>{a.author.name}</td>
