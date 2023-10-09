a5f598f113e9443de1dbae2eedf61639edf37241
Barbara <barbara.taskinen@gmail.com>
2023-10-09 16:33:20 +0300

    Exercise 9.5
    - bmi endpoint implemented
    - error handling not yet implemented

--- part9/BMI/index.ts
+++ part9/BMI/index.ts
@@ -1,9 +1,23 @@
 import express from 'express';
+import { calculateBmi } from './bmiCalculator';
 
 const app = express();
 
 app.get('/hello', (_req, res) => {
   res.send('Hello Full Stack!');
+});
+
+app.get('/bmi', (req, res) => {
+  console.log(req.query);
+  const height = Number(req.query.height);
+  const weight = Number(req.query.weight);
+  console.log(height, weight);
+  const bmi = calculateBmi(height, weight);
+  res.send({
+    weight: weight,
+    height: height,
+    bmi: bmi,
+  });
 });
 
 const PORT = 3003;
