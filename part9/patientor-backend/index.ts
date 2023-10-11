5c958dbac4eb046b622206571303511ef2c76360
Barbara <barbara.taskinen@gmail.com>
2023-10-11 11:10:07 +0300

    Exercise 9.9
    Set up frontend of patientor

--- part9/patientor-backend/index.ts
+++ part9/patientor-backend/index.ts
@@ -1,11 +1,12 @@
 import express from 'express';
+
 const app = express();
 
 app.use(express.json());
 
 const PORT = 3000;
 
-app.get('/ping', (_req, res) => {
+app.get('/api/ping', (_req, res) => {
   console.log('someone pinged here');
   res.send('pong');
 });
