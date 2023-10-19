7e6f632caac177dd485d6e812b1e67ddaf83e022
Barbara <barbara.taskinen@gmail.com>
2023-10-19 11:53:24 +0300

    Fix issues with git due to wrong cloning of patientor project

---
+++ part9/patientor_new/src/components/PatientInformationPage.tsx
@@ -0,0 +1,51 @@
+import { Typography } from '@mui/material';
+import { Patient } from '../types';
+import { useParams } from 'react-router-dom';
+import FemaleIcon from '@mui/icons-material/Female';
+import MaleIcon from '@mui/icons-material/Male';
+import './PatientInformationPage.css';
+
+type Props = {
+  patients: Patient[];
+};
+
+const PatientInformationPage = ({ patients }: Props) => {
+  const id = useParams().id;
+  const patient = patients.find((patient) => patient.id === id);
+
+  if (!patient) return <div>Patient not found.</div>;
+
+  let genderIcon = null;
+
+  switch (patient.gender) {
+    case 'female': {
+      genderIcon = <FemaleIcon className="genderSvg" />;
+      break;
+    }
+    case 'male': {
+      genderIcon = <MaleIcon className="genderSvg" />;
+      break;
+    }
+    default:
+      break;
+  }
+
+  return (
+    <div>
+      <div className="nameContainer">
+        <Typography
+          variant="h4"
+          style={{ marginBottom: '0.5em', marginTop: '0.5em' }}
+        >
+          {patient.name}
+        </Typography>
+        {genderIcon}
+      </div>
+
+      <Typography>ssh: {patient.ssn}</Typography>
+      <Typography>occupation: {patient.occupation}</Typography>
+    </div>
+  );
+};
+
+export default PatientInformationPage;
