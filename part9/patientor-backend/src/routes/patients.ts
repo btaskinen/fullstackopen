f3f5b05e0a9302a300c8a8cf70f923f1247f1df5
Barbara <barbara.taskinen@gmail.com>
2023-10-19 11:28:17 +0300

    Exercise 9.21
    - Created a page for showing a patient's full information in the frontend.

--- part9/patientor-backend/src/routes/patients.ts
+++ part9/patientor-backend/src/routes/patients.ts
@@ -5,7 +5,7 @@ import toNewPatient from '../utils';
 const router = express.Router();
 
 router.get('/', (_req, res) => {
-  res.send(patientsServices.getNonSensitivePatients());
+  res.send(patientsServices.getPatients());
 });
 
 router.get('/:id', (req, res) => {
